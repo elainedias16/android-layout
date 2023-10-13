f7d5ec6aae0851e64abbdfaeb3246e036a870134
elainedias16 <elainediaspires16@gmail.com>
2023-10-13 18:27:37 -0300

    feat : binding app citizen

--- app/src/main/java/com/example/android_layout/MainActivity.kt
+++ app/src/main/java/com/example/android_layout/MainActivity.kt
@@ -4,6 +4,7 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.app.Activity
 import android.view.View
+import android.widget.EditText
 import android.widget.Toast
 import com.example.android_layout.databinding.ActivityMainBinding
 
@@ -16,19 +17,42 @@ class MainActivity : AppCompatActivity(), View.OnClickListener {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
-        binding.buttonSave.setOnClickListener(this)
+//        binding.buttonSave.setOnClickListener(this)
+        binding.buttonAdicionar.setOnClickListener(this)
 
 
     }
 
     override fun onClick(view: View) {
-        if (view.id == R.id.button_save) {
-            val text = "O botão foi clicadooooooooooo!"
-            val duration = Toast.LENGTH_SHORT
-            val toast = Toast.makeText(applicationContext, text, duration)
-            toast.show()
+//        if (view.id == R.id.button_save) {
+//            val text = "O botão foi clicadooooooooooo!"
+//            val duration = Toast.LENGTH_SHORT
+//            val toast = Toast.makeText(applicationContext, text, duration)
+//            toast.show()
+//        }
+
+//        if (view.id == R.id.button_adicionar) {
+//            val editText = findViewById<EditText>(R.id.editText)
+//            val userInput = editText.text.toString()
+//            binding.buttonAdicionar.text = userInput
+//
+//       }
+
+        if (view.id == R.id.button_adicionar) {
+            val editText = findViewById<EditText>(R.id.editText_one)
+            val userInput = editText.text.toString()
+
+            // Use data binding to set the text of the TextView
+            binding.textHello.text = "Olá, $userInput"
         }
+
+
+
     }
+
+
+
+
 }
 
 //    override fun onClick(view: View) {
